7b0bafe11710365405a65394ee1a4e6bb9f0f6c1
Angela Hentschel <angela.neuron@gmail.com>
2022-09-30 09:23:16 +0200

    create fetch function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
+
 import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Card from './components/Card.jsx';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  async function fetchCharacters() {
+    try {
+      const response = await fetch('https://rickandmortyapi.com/api/character');
+      const data = await response.json();
+      //console.log(data.results);
+
+      const test = data.results.map((character) => {
+        return {
+          id: character.id,
+          name: character.name,
+          source: character.image,
+          status: character.status,
+          species: character.species,
+          gender: character.gender,
+          origin: character.origin.name,
+          location: character.location.name,
+        };
+      });
+
+      console.log(test);
+    } catch (error) {
+      console.error('Fetching data failed: ' + error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
   return (
     <div className="App">
       <Header />
